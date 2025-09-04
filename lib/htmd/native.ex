abc9c77457604c2e7e8f433d0f4ddb9579622b8d
Kasun Vithanage <alankasun@gmail.com>
2025-09-04 22:02:49 +0530

    feat: rustler precompiled

--- lib/htmd/native.ex
+++ lib/htmd/native.ex
@@ -1,5 +1,30 @@
 defmodule Htmd.Native do
-  use Rustler, otp_app: :htmd, crate: "htmd"
+  mix_config = Mix.Project.config()
+  version = mix_config[:version]
+  github_url = mix_config[:package][:links]["GitHub"]
+
+  targets = ~w(
+    aarch64-apple-darwin
+    aarch64-unknown-linux-gnu
+    aarch64-unknown-linux-musl
+    riscv64gc-unknown-linux-gnu
+    x86_64-apple-darwin
+    x86_64-pc-windows-gnu
+    x86_64-pc-windows-msvc
+    x86_64-unknown-linux-gnu
+    x86_64-unknown-linux-musl
+  )
+
+  nif_versions = ~w(2.17 2.16)
+
+  use RustlerPrecompiled,
+    otp_app: :htmd,
+    crate: "htmd",
+    base_url: "#{github_url}/releases/download/v#{version}",
+    nif_versions: nif_versions,
+    targets: targets,
+    version: version,
+    force_build: System.get_env("HTMD_BUILD") in ["1", "true"]
 
   alias Htmd.ConvertOptions
 
--- mix.exs
+++ mix.exs
@@ -1,16 +1,20 @@
 defmodule Htmd.MixProject do
   use Mix.Project
+  @version "0.1.0"
+  @source_url "https://github.com/kasvith/htmd"
 
   def project do
     [
       app: :htmd,
-      version: "0.1.0",
+      name: "Htmd",
+      version: @version,
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
       package: package(),
-      source_url: "https://github.com/kasvith/htmd"
+      docs: docs(),
+      source_url: @source_url
     ]
   end
 
@@ -24,7 +28,9 @@ defmodule Htmd.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, "~> 0.36.2", runtime: false}
+      {:rustler, "~> 0.36.2", optional: true},
+      {:rustler_precompiled, "~> 0.8"},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false, warn_if_outdated: true}
     ]
   end
 
@@ -36,8 +42,19 @@ defmodule Htmd.MixProject do
     [
       name: "htmd",
       maintainers: ["Kasun Vithanage"],
+      description: "A fast HTML to Markdown converter for Elixir, powered by Rust.",
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/kasvith/htmd"}
+      links: %{"GitHub" => "https://github.com/kasvith/htmd"},
+      files: ~w(lib native .formatter.exs README* LICENSE* mix.exs checksum-*.exs)
+    ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "v#{@version}",
+      extras: [{:"README.md", [title: "README"]}]
     ]
   end
 end
